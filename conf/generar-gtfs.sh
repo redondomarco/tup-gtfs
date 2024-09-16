41733d1d501b29d5df569aa1267a7773b08123c5
Marco Redondo <mredond1@rosario.gov.ar>
2024-09-16 14:06:16 -0300

    saco pgadmin4 y completa salida de generar-gtfs

--- conf/generar-gtfs.sh
+++ conf/generar-gtfs.sh
@@ -3,4 +3,12 @@ today=$(date +"%Y%m%d-%H%M%S")
 sh shape2psql.sh > /logs/shape2psql-${today}.log
 sh psql2gtfs.sh > /logs/psql2gtfs-${today}.log
 chmod 777 /logs
+echo ''
+echo 'generado..'
+ls -la /otp/Graph.obj
 
+echo 'validacion en'
+ls -lart /logs/validation-*
+
+echo 'logs en'
+ls -lart /logs/*.log
