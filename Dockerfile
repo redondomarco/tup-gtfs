5f11730b7c9d4514cd11aa26ab7cbef5c1858528
Marco Redondo <mredond1@rosario.gov.ar>
2024-09-26 12:06:57 -0300

    agrego descarga de pbf argentina + recorto + filtro

--- Dockerfile
+++ Dockerfile
@@ -6,24 +6,33 @@ COPY conf/debian-bullseye.list /etc/apt/sources.list
 
 # instalo paquetes extra necesarios para la ejecucion del proceso
 RUN apt-get update && apt-get install -y \
-gdal-bin python2 git vim curl wget bash-completion \
-postgis zip locales locales-all
+gdal-bin python2 git vim curl wget bash-completion axel \
+postgis zip locales locales-all \
+openjdk-17-jre osmosis osmctools osmium-tool
 
 #configuro locales para ejecucion de los sql en la misma codificacion
 ENV LANG=es_AR.UTF-8
 ENV LC_ALL=es_AR.UTF-8
 
 # descargo java8
-RUN wget https://download.java.net/openjdk/jdk8u44/ri/openjdk-8u44-linux-x64.tar.gz
+RUN axel -n 10 https://download.java.net/openjdk/jdk8u44/ri/openjdk-8u44-linux-x64.tar.gz
 
 # configuro java8
 RUN tar xvf openjdk-8u44-linux-x64.tar.gz
 
 # descargo otp
-RUN wget https://repo1.maven.org/maven2/org/opentripplanner/otp/1.2.0/otp-1.2.0-shaded.jar
+RUN axel -n 10 https://repo1.maven.org/maven2/org/opentripplanner/otp/1.2.0/otp-1.2.0-shaded.jar
 
 # limpieza
 
 RUN rm openjdk-8u44-linux-x64.tar.gz
 
 RUN rm -rf /var/lib/apt/lists/*
+
+
+RUN axel -n 10 https://repo1.maven.org/maven2/org/opentripplanner/otp/2.6.0/otp-2.6.0-shaded.jar
+
+RUN axel -n 10 https://repo1.maven.org/maven2/org/opentripplanner/otp/2.4.0/otp-2.4.0-shaded.jar
+
+
+#-60.9382,-33.175,-60.4493,-32.7233
--- conf/psql2gtfs.sh
+++ conf/psql2gtfs.sh
@@ -25,8 +25,28 @@ psql "host=pg_tup dbname=tup_gtfs user=${POSTGRES_USER}" -f gtfs-sql/stop-times.
 rm otp/gtfs.zip
 zip -j otp/gtfs.zip gtfs/*.txt
 
+rm /otp/*.pbf
+
 python2 /libs/transitfeed-1.2.16/feedvalidator.py -n otp/gtfs.zip -o /logs/validation-result_${today}.html
+
+# descargo mapa argentina
+axel -n 10 --output=/tmp/argentina-latest.osm.pbf http://download.geofabrik.de/south-america/argentina-latest.osm.pbf
+
+# recorto area rosario
+# https://boundingbox.klokantech.com/
+osmconvert /tmp/argentina-latest.osm.pbf -b=-60.931758,-33.044954,-60.611781,-32.813269 --complete-ways -o=/tmp/rosario-unfiltered.pbf
+#-60.9382,-33.175,-60.4493,-32.7233
+# filtro 1
+osmosis --rb /tmp/rosario-unfiltered.pbf --tf reject-ways building=* --tf reject-ways waterway=* --tf reject-ways landuse=* --tf reject-ways natural=* --used-node --wb /tmp/rosario-filtered1.pbf
+
+#filtro 2
+
+osmium tags-filter /tmp/rosario-filtered1.pbf w/highway wa/public_transport=platform wa/railway=platform w/park_ride=yes r/type=restriction r/type=route -o /otp/rosario-filtered2.pbf -f pbf,add_metadata=false
 
 /java-se-8u44-ri/bin/java -Xmx2G -jar /otp-1.2.0-shaded.jar --build otp/
 
+#java -Xmx2G -jar /otp-2.4.0-shaded.jar --build --save otp/ > /logs/otp_${today}.log
+
 #scp otp/Graph.obj tcatws1-test:/mnt/ubicaciones
+# http://tcatws1-test.pm.rosario.gov.ar:8080/ubicaciones/graph-info
+# https://t-comollego.rosario.gob.ar/
